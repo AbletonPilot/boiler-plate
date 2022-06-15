5774c76552d95078ff114d6304df6ed56ee00ea3
SON <son@SONs-MacBook-Pro.local>
2022-06-16 00:39:05 +0900

    1번째

---
+++ Models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+//유저에 대한 스키마 설정
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+//스키마를 모델로 감싸주는 설정
+const User = mongoose.model('User', userSchema)
+
+//다른 곳에서 사용할 수 있도록 설정
+module.exports ={ User }
