d1e4e90bc5af9a6aebe5369495d5c294609b51bb
SON <son@SONs-MacBook-Pro.local>
2022-06-17 01:22:22 +0900

    06.16.2022

--- Models/User.js
+++ Models/User.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//salt를 이용해서 비밀번호를 암호화 해야한다. 그 중 salt의 문자 길이
+const saltRounds = 10
+
+const jwt = require('jsonwebtoken');
 
 //유저에 대한 스키마 설정
 const userSchema = mongoose.Schema({
@@ -13,11 +19,11 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 50
+        minlength: 5
     },
     lastname: {
         type: String,
-        minlength: 5
+        maxlength: 50
     },
     role: {
         type: Number,
@@ -31,6 +37,55 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+//유저 모델의 정보를 저장하기 전에 fungtion을 한다
+userSchema.pre('save', function(next) {
+    //user = userSchema
+    var user = this;
+    //비밀번호를 바꿀때만 암호화 시키는 조건
+    if(user.isModified('password')) {
+        //비밀번호를 암호화 시킨다.
+        //비크립트 가져오고 솔트 생성, 솔트 = 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                //패스워드 암호화 성공하면 유저 패스워드를 hash로 교체
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+//로그인 시 비밀번호 확인
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword 유저의 비밀번호와 암호화된 비밀번호가 같은지 확인한다.
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        //비밀번호가 맞지 않다면 리턴해서 cb
+        if(err) return cb(err);
+        //비밀번호가 맞다면 cb
+        cb(null, isMatch);
+    })
+}
+
+//로그인 시 토큰 생성
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    //jsonwebtoken을 이용해서 token을 생성하기
+    //유저의 아이디와 secretToken을 합쳐서 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user) {
+        //에러가 있다면 return, cb로 에러 전달
+        if(err) return cb(err)
+        //save가 잘 됬으면 유저 정보 전달
+        cb(null, user)
+    })
+}
 
 //스키마를 모델로 감싸주는 설정
 const User = mongoose.model('User', userSchema)
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const port = 3000
 
 const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require("./Models/User");
@@ -14,6 +15,7 @@ app.use(bodyparser.urlencoded({extended: true}));
 
 //application/json
 app.use(bodyparser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -27,13 +29,41 @@ app.get('/', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  //회원 가입 할 때 필요한 정보들을  Client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+  //회원 가입할 때 필요한 정보들을  Client에서 가져오면 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body)
-
+  
   user.save((err, userInfo) => {
     if(err) return res.json({ success: false, err })
     return res.status(200).json({
       success:true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    //이메일을 가진 유저가 한명도 없다면 user가 없다. 그럴때 return
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+    user.comparePassword(req.body.password , (err, isMatch ) => {
+      //비밀번호가 틀리다면
+      if(!isMatch)
+       return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+      //비밀번호까지 맞다면 토큰 생성한다.
+      user.generateToken((err, user) => {
+        //에러가 있다면 400 에러 전달
+        if(err) return res.status(400).send(err);
+        //쿠키, 로컬스토리지 등 토큰을 저장한다. (지금은 쿠키)
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
     })
   })
 })
