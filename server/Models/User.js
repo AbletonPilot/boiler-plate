5a36f174cf2af51d18ee90950616194d51722e26
SON <son@SONs-MacBook-Pro.local>
2022-06-17 23:52:51 +0900

    06.17.2022

--- server/Models/User.js
+++ server/Models/User.js
@@ -87,6 +87,20 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+  
+    //토큰을 가져온다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+      //유저 아이디를 이용해서 유저를 찾은 다음에
+      //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+      user.findOne({ "_id": decoded, "token": token }, function(err, user) {
+        if(err) return cb(err);
+        cb(null, user)
+      })
+    })
+  }
+  
 //스키마를 모델로 감싸주는 설정
 const User = mongoose.model('User', userSchema)
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 
-//http://localhost3000
-const port = 3000
+//http://localhost5000
+const port = 5000
 
 const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
+const { auth } = require('./middleware/auth');
 const { User } = require("./Models/User");
 
 //application/x-www-form-urlencoded
@@ -28,7 +29,11 @@ app.get('/', (req, res) => {
   res.send('ㅎㅇ')
 })
 
-app.post('/register', (req, res) => {
+app.get('/api/hello', (req, res) => {
+  res.send("안녕하세요")
+})
+
+app.post('/api/users/register', (req, res) => {
   //회원 가입할 때 필요한 정보들을  Client에서 가져오면 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body)
   
@@ -40,7 +45,7 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     //이메일을 가진 유저가 한명도 없다면 user가 없다. 그럴때 return
@@ -67,6 +72,35 @@ app.post('/login', (req, res) => {
     })
   })
 })
+
+// role 1 어드민    role 2 특정 부서 어드민 
+// role 0 -> 일반유저   role 0이 아니면  관리자 
+app.get('/api/users/auth', auth, (req, res) => {
+  //여기 까지 미들웨어를 통과해 왔다는 얘기는  Authentication 이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  // console.log('req.user', req.user)
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
